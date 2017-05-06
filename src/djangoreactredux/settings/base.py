14e0046e0535563fb78d8d0271f1f802ce86bf25
Léo Martinez <leo84.martinez@gmail.com>
2017-05-06 15:05:11 +0200

    Secure API endpoints

--- src/djangoreactredux/settings/base.py
+++ src/djangoreactredux/settings/base.py
@@ -88,10 +88,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ############# REST FRAMEWORK ###################
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        'knox.auth.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
