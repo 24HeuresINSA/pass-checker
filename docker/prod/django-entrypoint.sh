3ddf186c44ed7187634d39b75ade768b844c4a73
Léo Martinez <leo84.martinez@gmail.com>
2017-05-06 18:02:30 +0200

    Disable auto fixture load on prod

--- docker/prod/django-entrypoint.sh
+++ docker/prod/django-entrypoint.sh
@@ -11,6 +11,5 @@ do
     sleep 2
 done
 
-python manage.py loaddata fixtures.json --settings=djangoreactredux.settings.prod
 /etc/init.d/nginx start
 gunicorn --bind=0.0.0.0:8000 --workers=3 --env DJANGO_SETTINGS_MODULE=djangoreactredux.settings.prod djangoreactredux.wsgi
